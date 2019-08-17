5f0181584f08b94c266b3c585e440e4aec84c4c3
ZacChristensen31 <zac.d.christensen@gmail.com>
2019-08-17 17:22:41 -0500

    Starting up POC for some stuff to Dynamo

---
+++ poc/test.py
@@ -0,0 +1,6 @@
+'''
+
+Pretty much just testing calls to the AWS stuff, mostly Dynamo at this point.
+Will transfer this to LogarunToWULoggerPipeline when this is done, but want to keep things "clean" in that folder
+
+'''
