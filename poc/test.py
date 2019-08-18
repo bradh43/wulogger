1a71b3c330e681b30a66dedc46cd1e27891fa14d
ZacChristensen31 <zac.d.christensen@gmail.com>
2019-08-17 19:45:06 -0500

    setting things up to test and rework everything

--- poc/test.py
+++ poc/test.py
@@ -30,14 +30,14 @@ athlete_info = {
 
 log_info = {
 	"title": "Fun run",
-	"note":	"Had a neat run",
-	"date":	"01-31-2019",
+	"note": "Had a neat run",
+	"date": "01-31-2019",
 	"author": "Prat",
 	"sleep_hr": "8.0",
 	"last_updated_timestamp": "2019-08-07T18:21:00.000Z",
 	"activity_list": [
 		{
-			"type":	"swim",
+			"type": "swim",
 			"duration": "4147.2",
 			"distance": "6.3",
 			"distance_unit": "km",
@@ -48,94 +48,101 @@ log_info = {
 		{
 			"comment": "cool bro",
 			"timestamp": "2019-07-05T14:48:00.000Z",
-			"user":	"Joe"
+			"user": "Joe"
 		},
 	],
 }
 
-def null_safe_field(dictionary, field):
-	return dictionary['field'] if field in dictionary.keys() else "None"
 
-def get_shoe_info(shoe):
-	return dict(name={'S': shoe['name']},
-				distance={'N': shoe['distance']},
-				distace_unit={'S': shoe['distance_unit']},
-				mileage_limit={'N': shoe['mileage_limit']},
-				status={'S': shoe['status']})
+class Test:
 
-def put_new_athlete(input_data):
-	firstName = {'S': input_data['first_name']}
-	lastName = {'S': input_data['last_name']}
-	teamList = {'SS': input_data['team_list']}
-	birthdate = {'S': input_data['birthdate']}
-	displayName = {'S': input_data['display_name']}
-	email = {'S': input_data['email']}
-	private = {'BOOL': input_data['private']}
-	password = {'S': input_data['password']}
-	profilePictureUrl = {'S': input_data['profile_picture_url']}
-	shoeList = {'L': [{'M': get_shoe_info(shoe)} for shoe in input_data['shoe_list']]}
+	def __init__(self):
+		self.what = 'what'
 
-	postItem = dict(first_name=firstName,
-					last_name=lastName,
-					team_list=teamList,
-					birthdate=birthdate,
-					display_name=displayName,
-					email=email,
-					private=private,
-					password=password,
-					profile_picture_url=profilePictureUrl,
-					shoe_list=shoeList)
+	def null_safe_field(self, dictionary, field):
+		return dictionary['field'] if field in dictionary.keys() else "None"
 
-	client = boto3.client('dynamodb')
+	def get_shoe_info(self, shoe):
+		return dict(name={'S': shoe['name']},
+		            distance={'N': shoe['distance']},
+		            distace_unit={'S': shoe['distance_unit']},
+		            mileage_limit={'N': shoe['mileage_limit']},
+		            status={'S': shoe['status']})
 
-	response = client.put_item(
-		TableName = 'Athletes',
-		Item = postItem,
-		ReturnValues = 'ALL_OLD',
-		ReturnConsumedCapacity = 'NONE'
-	)
+	def put_new_athlete(self, input_data):
+		firstName = {'S': input_data['first_name']}
+		lastName = {'S': input_data['last_name']}
+		teamList = {'SS': input_data['team_list']}
+		birthdate = {'S': input_data['birthdate']}
+		displayName = {'S': input_data['display_name']}
+		email = {'S': input_data['email']}
+		private = {'BOOL': input_data['private']}
+		password = {'S': input_data['password']}
+		profilePictureUrl = {'S': input_data['profile_picture_url']}
+		shoeList = {'L': [{'M': self.get_shoe_info(shoe)} for shoe in input_data['shoe_list']]}
 
-	return response
+		postItem = dict(first_name=firstName,
+		                last_name=lastName,
+		                team_list=teamList,
+		                birthdate=birthdate,
+		                display_name=displayName,
+		                email=email,
+		                private=private,
+		                password=password,
+		                profile_picture_url=profilePictureUrl,
+		                shoe_list=shoeList)
 
-def get_activity(activity):
-	return dict(
-		type = {'S' : activity['type']},
-		duration = {'N' : activity['duration']},
-		distance = {'N' : activity['distance']},
-		distance_unit = {'S' : activity['distance_unit']},
-		shoe = {'S' : null_safe_field(activity, 'shoe')}
-	)
+		client = boto3.client('dynamodb')
 
-def get_comment(comment):
-	return dict(
-		comment = {'S' : comment['comment']},
-		timestamp = {'S' : comment['timestamp']},
-		user = {'S' : comment['user']}
-	)
+		response = client.put_item(
+			TableName='Athletes',
+			Item=postItem,
+			ReturnValues='ALL_OLD',
+			ReturnConsumedCapacity='NONE'
+		)
 
-def put_new_log(input_data):
+		return response
 
-	postItem = dict(
-		title={'S': input_data['title']},
-		note={'S': input_data['note']},
-		date={'S': input_data['date']},
-		author={'S': input_data['author']},
-		sleep_hr={'N': input_data['sleep_hr']},
-		last_updated_timestamp={'S': input_data['last_updated_timestamp']},
-		activity_list={'L': [{'M' : get_activity(activity)} for activity in input_data['activity_list']]},
-		like_list={'SS': input_data['like_list']},
-		comment_list={'L': [{'M' : get_comment(comment)} for comment in input_data['comment_list']]}
-	)
+	def get_activity(self, activity):
+		return dict(
+			type={'S': activity['type']},
+			duration={'N': activity['duration']},
+			distance={'N': activity['distance']},
+			distance_unit={'S': activity['distance_unit']},
+			shoe={'S': self.null_safe_field(activity, 'shoe')}
+		)
 
-	client = boto3.client('dynamodb')
+	def get_comment(self, comment):
+		return dict(
+			comment={'S': comment['comment']},
+			timestamp={'S': comment['timestamp']},
+			user={'S': comment['user']}
+		)
 
-	response = client.put_item(
-		TableName='Logs',
-		Item=postItem,
-		ReturnValues='ALL_OLD',
-		ReturnConsumedCapacity='NONE'
-	)
+	def put_new_log(self, input_data):
+		postItem = dict(
+			title={'S': input_data['title']},
+			note={'S': input_data['note']},
+			date={'S': input_data['date']},
+			author={'S': input_data['author']},
+			sleep_hr={'N': input_data['sleep_hr']},
+			last_updated_timestamp={'S': input_data['last_updated_timestamp']},
+			activity_list={'L': [{'M': self.get_activity(activity)} for activity in input_data['activity_list']]},
+			like_list={'SS': input_data['like_list']},
+			comment_list={'L': [{'M': self.get_comment(comment)} for comment in input_data['comment_list']]}
+		)
+
+		client = boto3.client('dynamodb')
+
+		response = client.put_item(
+			TableName='Logs',
+			Item=postItem,
+			ReturnValues='ALL_OLD',
+			ReturnConsumedCapacity='NONE'
+		)
+
 
 if __name__ == '__main__':
-	put_new_athlete(athlete_info)
-	put_new_log(log_info)
+	hold = test()
+	test.put_new_athlete(athlete_info)
+	test.put_new_log(log_info)
---
+++ poc/test_test.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+
+class TestTest(TestCase):
+	def test_null_safe_field(self):
+		self.fail()
+
+	def test_get_shoe_info(self):
+		self.fail()
+
+	def test_put_new_athlete(self):
+		self.fail()
+
+	def test_get_activity(self):
+		self.fail()
+
+	def test_get_comment(self):
+		self.fail()
+
+	def test_put_new_log(self):
+		self.fail()
+
